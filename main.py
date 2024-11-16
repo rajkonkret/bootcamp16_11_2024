528ce094cbb75135a5bc0c5d1270dc129f62d5b2
rjaniak <rajkonkret660@gmail.com>
2024-11-16 12:17:55 +0100

    [1 1] update main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 2 x shift - podręczne menu wyszukiwania
+# theme - skórki, wygląd, kolor pycharma
+# wheel - zmiana wielkości liter kółkiem myszy
+# Hi, PyCharm
+# ctrl shift f10 - uruchamianie programów
+# (.venv) PS C:\Users\Administrator\PycharmProjects\bootcamp16_11_2024> python .\main.py
+# Hi, PyCharm
+# (.venv) PS C:\Users\Administrator\PycharmProjects\bootcamp16_11_2024>
