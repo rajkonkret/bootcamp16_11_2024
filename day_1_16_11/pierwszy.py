9b5256a906b6a095b4dd58c524357c230edd2526
rjaniak <rajkonkret660@gmail.com>
2024-11-16 12:31:31 +0100

    [1 2] add pierwszy.py

---
+++ day_1_16_11/pierwszy.py
@@ -0,0 +1,7 @@
+# PEP8 - https://peps.python.org/pep-0008/
+# PEP 8 – Style Guide for Python Code
+# https://kariera.comarch.pl/blog/clean-code-15-krokow-do-stworzenia-czystego-kodu/
+# snake_case - konwencja nazewnicza dla nazw plikó, zmiennych, funkcji
+
+print()  # wydrukuj/wypisz
+# ctrl alt l
