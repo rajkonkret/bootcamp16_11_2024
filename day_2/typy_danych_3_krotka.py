0c6e35b092092c8681d6157f5e3bd52608245695
rjaniak <rajkonkret660@gmail.com>
2024-11-17 14:32:53 +0100

    [2 8] update typy_danych_3_krotka.py

--- day_2/typy_danych_3_krotka.py
+++ day_2/typy_danych_3_krotka.py
@@ -72,3 +72,56 @@ print(sorted(tupla_names))  # ['Bartek', 'Radek', 'Tomek', 'Zenek']
 print(sorted(tupla_names, reverse=True))  # ['Zenek', 'Tomek', 'Radek', 'Bartek']
 
 print(tupla_names)  # ('Radek', 'Tomek', 'Zenek', 'Bartek')
+
+# rozpakowanie krotki
+a, b = 1, 2  # przypisze do zmiennych wartości wg kolejności
+print(f"{a=} {b=}")  # a=1 b=2
+print(type((1, 2)))  # <class 'tuple'>
+# a, b = 1, 2, 3 # ValueError: too many values to unpack (expected 2)
+a, *b = 1, 2, 3  # * worek na pozostałe dane
+print(f"{a=} {b=}")  # a=1 b=[2, 3]
+
+# ('Radek', 'Tomek', 'Zenek', 'Bartek')
+# name1, name2, name3
+# * moze byc tylko w jednym miejscu
+*name1, name2, name3 = tupla_names
+print(f"{name1=} {name2=} {name3=}")
+# name1=['Radek', 'Tomek'] name2='Zenek' name3='Bartek'
+
+name1, *name2, name3 = tupla_names
+print(f"{name1=} {name2=} {name3=}")
+# name1='Radek' name2=['Tomek', 'Zenek'] name3='Bartek'
+
+name1, name2, *name3 = tupla_names
+print(f"{name1=} {name2=} {name3=}")
+# name1='Radek' name2='Tomek' name3=['Zenek', 'Bartek']
+
+tupla_zadanie = "OLA", 'Ania', "Ada", "Gabi", "Kasia", "Paulina"
+i1, i2, *i3, i4 = tupla_zadanie
+print(i1, i2, i3, i4)  # OLA Ania ['Ada', 'Gabi', 'Kasia'] Paulina
+"""
+    Prints the values to a stream, or to sys.stdout by default.
+
+      sep
+        string inserted between values, default a space.
+      end
+        string appended after the last value, default a newline.
+      file
+        a file-like object (stream); defaults to the current sys.stdout.
+      flush
+        whether to forcibly flush the stream.
+    """
+print("Jeden", "Dwa", "Trzy")  # Jeden Dwa Trzy
+print("Jeden", "Dwa", "Trzy", sep="")  # "" - pusty separator, JedenDwaTrzy
+print("Jeden", "Dwa", "Trzy", sep="=>")  # "" - pusty separator, Jeden=>Dwa=>Trzy
+print("Jeden", "Dwa", "Trzy", sep=":")  # "" - Jeden:Dwa:Trzy
+print("Jeden", "Dwa", "Trzy", sep=":", end="")  # "" - pusty znak konca linii, Jeden:Dwa:Trzy
+print("Dalszy tekst")  # Jeden:Dwa:TrzyDalszy tekst wypisany bez zmiany do nowej linii
+print("Radek")  # Radek, w nowej linii bo poprzednia linia ustawiła na domyślny "\n"
+# sep: znak oddzielający elelementy (domyślnie " ")
+# end: znak na końcu linii (domyślnie "\n" - nowa linia)
+
+lista = list(tupla_names)
+print(lista)  # ['Radek', 'Tomek', 'Zenek', 'Bartek']
+print(type(lista))  # <class 'list'>
+print(len(lista))  # długość 4
