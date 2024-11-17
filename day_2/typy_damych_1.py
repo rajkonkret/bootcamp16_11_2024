54275bda611c1607d9a89b8f137679beac81fe05
rjaniak <rajkonkret660@gmail.com>
2024-11-17 09:48:12 +0100

    [2 1] add typy_damych_1.py

---
+++ day_2/typy_damych_1.py
@@ -0,0 +1,68 @@
+import sys
+
+wiek = 47  # int
+rok = 2024  # int
+temp = 36.6  # float
+
+print(temp)
+print(type(temp))  # <class 'float'>
+
+print(wiek + rok)  # 2071
+print(wiek - rok)  # -1977
+print(wiek * rok)  # 95128
+print(wiek / rok)  # 0.023221343873517788
+
+print(rok // wiek)  # częśc całkowita z dzielenia, 43
+print(5 // 2)  # 2
+
+print(rok % wiek)  # modulo, reszta z dzielenia 3
+print(3 % 2)  # 3 //2 = 1 to 1 * 2 = 2, reszta 3 - 2 = 1
+
+print(wiek ** rok)
+print(f"{wiek ** rok:,}")
+
+wynik = wiek ** rok
+print(len(str(wynik)))  # długość 3385 cyfr
+# print(len(str(wynik ** 2))) #
+# ValueError: Exceeds the limit (4300 digits) for integer string conversion;
+# use sys.set_int_max_str_digits() to increase the limit
+
+print(74 - 8 * 45 + 8 / 2 + 8 / 2)  # -278.0
+print(74 - (8 * 45) + 8 / 2 + 8 / 2)  # -278.0
+print(74 - (8 * 45) + (8 / 2 + 8) / 2)  # -280.0
+
+# liczby float
+# liczby zmiennoprzecinkowe
+print(0.2 + 0.8)  # float 1.0
+print(0.2 + 0.7)  # 0.8999999999999999
+print(0.1 + 0.2)  # 0.30000000000000004
+# 12.345 + 1.0001 = 13.3451 might be rounded to 13.345.
+# float jest zapamiętywany w komputerze w sposób wykładniczy
+# a * 2 ^ n
+# z tego wynika bład zaokraglenia
+print(f"{0.2 + 0.7:.1f}")  # 0.9
+print(sys.float_info)
+# sys.float_info(
+#     max=1.7976931348623157e+308,
+#     max_exp=1024,
+#     max_10_exp=308,
+#     min=2.2250738585072014e-308,
+#     min_exp=-1021,
+#     min_10_exp=-307,
+#     dig=15,
+#     mant_dig=53
+#     , epsilon=2.220446049250313e-16, radix=2, rounds=1)
+
+print(f"Sprawdzenie zmiennej temp {temp}, wiek {wiek}")
+# Sprawdzenie zmiennej temp 36.6, wiek 47
+
+print(f'''
+    {wiek}
+    {temp}
+''')
+# "
+#     47
+#     36.6"
+
+print(type(4 / 2))  # zawsze daje <class 'float'>
+print(4 / 2)  # 2.0
