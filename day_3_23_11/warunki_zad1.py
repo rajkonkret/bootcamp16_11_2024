11deb4c8806ac097f8fe876e130d21064a70d9cf
rjaniak <rajkonkret660@gmail.com>
2024-11-23 14:04:43 +0100

    [3 9] update warunki_zad1.py

--- day_3_23_11/warunki_zad1.py
+++ day_3_23_11/warunki_zad1.py
@@ -27,3 +27,55 @@ print("Dalsza część programu")
 # Brawo
 # Brawo
 # Dalsza część programu
+
+odp = "Radek"
+if odp == "Radek":
+    print("Radek")
+# Radek
+
+odp = "Tomek"
+if odp == "Radek":
+    print("Radek")
+else:  # działanie domyślne, gdy warunek niespełniony
+    print("Inny przypadek")
+# Inny przypadek
+
+if True:
+    pass  # nic nie rób
+
+print("Dalej")
+
+# kolejność warunków ma znaczenie
+# po pierwszyw spełnionym warunku (True) wychodzi z drzewka
+# podatek = 0.9
+# zarobki = int(input("Podaj dochód"))
+# if zarobki < 10000:
+#     podatek = 0.0
+# elif zarobki < 30000:  # od 10000 do 29999
+#     podatek = 0.2
+# elif zarobki < 100000:
+#     podatek = 0.4
+# else:
+#     podatek = 0.9
+# print(f"Płacisz {zarobki * podatek} podatku")
+# # dodac podatek 0.2 dla przedziału 10000 do 29999
+
+# # tu ma znaczenie ostatni spełniony
+# if zarobki < 10000:
+#     podatek = 0
+#
+# if zarobki < 1000:
+#     podatek = 0.4
+
+suma_zam = 250
+if suma_zam > 150:
+    rabacik = 25
+else:
+    rabacik = 0
+
+print(f"Rabacik wynosi {rabacik}")
+
+rabat = 25 if suma_zam > 150 else 0 # ternary operator, operator warunkowy
+print(f"Rabacik wynosi {rabat}")
+# Rabacik wynosi 25
+# Rabacik wynosi 25
