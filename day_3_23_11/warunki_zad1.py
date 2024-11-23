a2c4508727f2c2007982c245bb849c239594a3eb
rjaniak <rajkonkret660@gmail.com>
2024-11-23 12:58:27 +0100

    [3 8] add warunki_zad1.py

---
+++ day_3_23_11/warunki_zad1.py
@@ -0,0 +1,29 @@
+# instrukcje warunkowe
+# instrukcje sterowania przepływem programu
+# if - sterowana warunkiem
+# if warunek:
+#    komenda(blok) wykonywany gdy warunek spełniony
+
+odp = True
+print(bool(odp))  #
+# odp = False
+# debug - tryb do uruchaminia programu linijka po linijce i sprawdzaniu parametrów
+if odp:
+    # po dwukropku wcięcie
+    # 4 spacje
+    print("Brawo")  # Brawo
+    print("Brawo")  # Brawo
+    print("Brawo")  # Brawo
+    print("Brawo")  # Brawo
+    print("Brawo")  # Brawo
+    print("Brawo")  # Brawo
+
+print("Dalsza część programu")
+# True
+# Brawo
+# Brawo
+# Brawo
+# Brawo
+# Brawo
+# Brawo
+# Dalsza część programu
