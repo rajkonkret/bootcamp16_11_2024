8b20069245d538cc92a751a0fbbb3d72471d8dc8
rjaniak <rajkonkret660@gmail.com>
2024-11-24 14:53:21 +0100

    [4 12] update pliki csv zad 1

--- day_4_24_11/pliki_csv_zad1.py
+++ day_4_24_11/pliki_csv_zad1.py
@@ -25,5 +25,21 @@ with open("dane/records_2.csv", 'w', newline="") as csv_f:
 
 with open("dane/records_3.csv", 'w', newline="") as csv_f:
     csv_dict_writer = csv.DictWriter(csv_f, fieldnames=fields)
-    csv_dict_writer.writeheader() # zapisz nazwy kolumn
-    csv_dict_writer.writerow(zipped_dict)
+    csv_dict_writer.writeheader()  # zapisz nazwy kolumn
+    csv_dict_writer.writerow(zipped_dict)  # zapis jedego słownika jako wiersz
+
+products = [
+    {'sku': 1, 'exp_date': 'today', 'price': 100.00},
+    {'sku': 2, 'exp_date': 'today', 'price': 200.00},
+    {'sku': 3, 'exp_date': 'tomorrow', 'price': 250},
+    {'sku': 4, 'exp_date': 'today', 'price': 50},
+    {'sku': 5, 'exp_date': 'today', 'price': 500.00},
+]
+# wyciągnięcie nazw kolumn ze słownika
+# products[0] wyciąga z listy pojedynczy słownik
+fields_product = [k for k in products[0]]
+
+with open("dane/records_discount.csv", 'w', newline="") as csv_f:
+    csv_dict_writer = csv.DictWriter(csv_f, fieldnames=fields_product, delimiter=";")
+    csv_dict_writer.writeheader()  # zapisz nazwy kolumn
+    csv_dict_writer.writerows(products)  # s - zapis listy słownikó jako kolejne wiersze csv
