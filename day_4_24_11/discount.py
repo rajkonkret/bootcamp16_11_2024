76675be819fa4389cc4b475962fcdc1544108863
rjaniak <rajkonkret660@gmail.com>
2024-11-24 11:35:26 +0100

    [4 6] update discount.py

--- day_4_24_11/discount.py
+++ day_4_24_11/discount.py
@@ -28,3 +28,38 @@ print("Aktualna godzina", formatted_time.removeprefix("0"))  # Aktualna godzina 
 #                 milliseconds=0, minutes=0, hours=0, weeks=0
 tomorrow = today + timedelta(days=1)
 print(tomorrow)  # 2024-11-25
+products = [
+    {'sku': 1, 'exp_date': today, 'price': 100.00},
+    {'sku': 2, 'exp_date': today, 'price': 200.00},
+    {'sku': 3, 'exp_date': tomorrow, 'price': 250},
+    {'sku': 4, 'exp_date': today, 'price': 50},
+    {'sku': 5, 'exp_date': today, 'price': 500.00},
+]
+
+# print(products)
+for product in products:
+    # print(product)  # {'sku': 5, 'exp_date': datetime.date(2024, 11, 24), 'price': 500.0}
+    # print(product['exp_date'])  # 2024-11-24
+
+    if product['exp_date'] != today:
+        continue  # konczy bieżąće wykonanie pętli, bierze kolejny element
+        # pass # nic nie rób, idź dalej
+    product['price'] *= 0.7  # price = price * 0.7
+    print(f"""
+Price for sku {product['sku']}
+is now {product['price']}
+""")
+# Price for sku 1
+# is now 70.0
+#
+#
+# Price for sku 2
+# is now 140.0
+#
+#
+# Price for sku 4
+# is now 35.0
+#
+#
+# Price for sku 5
+# is now 350.0
