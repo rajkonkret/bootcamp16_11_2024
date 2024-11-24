0b36606719cc801d751b87a2ded67e1b01ba14e2
rjaniak <rajkonkret660@gmail.com>
2024-11-24 14:29:46 +0100

    [4 11] add pliki csv zad 1

---
+++ day_4_24_11/pliki_csv_zad1.py
@@ -0,0 +1,29 @@
+# pliki csv - dane oddzielone znakiem podziału
+# ,;tab
+# imię,opis,ocena
+# Andrzej Nowak,fajny,4
+# "Jan Wiśniewski","fajny","2"
+# Kowalski,"wiecznie pyta ""która godzina"", ale może być",5
+import csv
+
+row = ['Radek', 'Coe', '3', '0']
+fields = ['name', 'branch', 'year', 'cgpa']
+
+zipped_dict = dict(zip(fields, row))
+print(zipped_dict)  # {'name': 'Radek', 'branch': 'Coe', 'year': '3', 'cgpa': '0'}
+
+# with open("dane\\records.csv", 'w') as csv_f: - dla windows
+with open("dane/records.csv", 'w', newline="") as csv_f:
+    csvwriter = csv.writer(csv_f)  # narzędzie do zapisu plików csv
+    csvwriter.writerow(row)
+
+# zapis kilku wierszy
+with open("dane/records_2.csv", 'w', newline="") as csv_f:
+    csvwriter = csv.writer(csv_f)  # narzędzie do zapisu plików csv
+    csvwriter.writerow(fields)
+    csvwriter.writerow(row)
+
+with open("dane/records_3.csv", 'w', newline="") as csv_f:
+    csv_dict_writer = csv.DictWriter(csv_f, fieldnames=fields)
+    csv_dict_writer.writeheader() # zapisz nazwy kolumn
+    csv_dict_writer.writerow(zipped_dict)
