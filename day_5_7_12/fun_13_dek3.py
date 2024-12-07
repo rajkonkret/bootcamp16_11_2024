99651ba076f783785d021ecfdf80d80236a2ca33
rjaniak <rajkonkret660@gmail.com>
2024-12-07 14:32:37 +0100

    [5 15] add fun13

---
+++ day_5_7_12/fun_13_dek3.py
@@ -0,0 +1,19 @@
+def sub_decorator(func):
+    def wrapper(num1, num2):
+        if num1 < num2:
+            num1, num2 = num2, num1
+            return func(num1, num2)
+
+    return wrapper
+
+
+@sub_decorator
+def substract(num1, num2):
+    res = num1 - num2
+    print("Result is:", res)
+
+
+substract(2, 4)  # Result is: 2
+a, b = 1, 2
+a, b = b, a
+print(a, b)  # 2 1
