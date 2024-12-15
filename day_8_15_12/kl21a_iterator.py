2309986f0e9c4bdc6101b05cb11f360a4ea00e2b
rjaniak <rajkonkret660@gmail.com>
2024-12-15 11:29:43 +0100

    [8 6] update kl21a.py

--- day_8_15_12/kl21a_iterator.py
+++ day_8_15_12/kl21a_iterator.py
@@ -1,6 +1,17 @@
 # iteratory - pozwala w sposób sekwencyjny dostęp do danych
 # oszczędza zużycie pamięci
 # zapamiętuje gdzie zakończylismy
+### Zalety Używania Iteratorów
+
+# 1. **Zarządzanie Pamięcią**: Iteratory są efektywne pod względem pamięci,
+# ponieważ nie wymagają wczytywania całego zbioru danych do pamięci na raz.
+# Są one szczególnie użyteczne przy przetwarzaniu dużych zbiorów danych.
+#
+# 2. **Uniwersalność**: Można je stosować do różnych typów struktur danych,
+# ułatwiając pisanie generycznego, wielokrotnego użytku kodu.
+#
+# 3. **Leniwe Wykonanie**: Iteratory realizują leniwe wykonanie,
+# co oznacza, że generują elementy na żądanie, co może być korzystne dla wydajności.
 
 lista = [1, 2, 3, 4, 5]
 print(lista)
@@ -48,3 +59,23 @@ class Count:
 print("-------")
 counter = Count(1, 20)
 print(next(counter))  # 1
+print(next(counter))  # 2
+print(next(counter))  # 3
+print(next(counter))  # 4
+
+print("-----")
+while True:
+    try:
+        number = next(counter)
+        print(number)
+    except StopIteration:
+        break
+
+# 5
+# 6
+# 7
+
+counter2 = Count(1, 20)
+print(next(counter2))  # 1
+print(next(counter2))  # 2
+print(next(counter2))  # 3
