021fdebae9219cc22845392bd6c0df3d06d03f04
rjaniak <rajkonkret660@gmail.com>
2025-01-04 16:00:22 +0100

    [9 15] add baza_da

---
+++ day_9_4_01_25/baza1.py
@@ -0,0 +1,8 @@
+# sqlite3 test.db  - otworzenie bazy, stworzy nową gdy nie ma
+# https://www.heidisql.com
+# https://sqlitebrowser.org/dl/
+# tworzenie tabeli w bazie danych (sql)
+# create table users (id INT PRIMARY KEY, name VARCHAR(100), age INT);
+# pokazuje tabele w bazie danych za pomoca silnika sqlite3
+# sqlite> .tables  - wyswietla liste tabel
+# users
