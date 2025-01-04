0da98da25ae9e6ed628546a526f264ef96888d71
rjaniak <rajkonkret660@gmail.com>
2025-01-04 10:04:30 +0100

    [9 2] update api randomuser

--- day_9_4_01_25/api_randomuser.py
+++ day_9_4_01_25/api_randomuser.py
@@ -1,4 +1,5 @@
 import requests as re
+from pydantic import BaseModel
 
 url = "https://randomuser.me/api/"
 
@@ -26,4 +27,45 @@ with open("user_photo.jpg", "wb") as f:
     f.write(response_photo.content)
 print("Zdjęcie zostało zapisane")
 
+
 # zbudować schemat klas dla name(first, last), email, picture(large)
+class Name(BaseModel):
+    title: str
+    first: str
+    last: str
+
+
+class Picture(BaseModel):
+    large: str
+
+
+class UserInfo(BaseModel):
+    # name: dict
+    name: Name
+    email: str
+    picture: Picture
+
+
+user = data['results'][0] # wyciągnięcie usera z listy
+user_info = UserInfo(**user) # rozpakowanie słownika na obiekty
+print(user_info)
+# name = {'title': 'Mademoiselle', 'first': 'Emmanuelle', 'last': 'Marie'}
+# email = 'emmanuelle.marie@example.com'
+# picture = {'large': 'https://randomuser.me/api/portraits/women/78.jpg',
+#            'medium': 'https://randomuser.me/api/portraits/med/women/78.jpg',
+#            'thumbnail': 'https://randomuser.me/api/portraits/thumb/women/78.jpg'}
+
+# Po uzupełnieniu modelu klas
+# name = Name(title='Mr', first='Helge', last='Lillegård')
+# email = 'helge.lillegard@example.com'
+# picture = Picture(large='https://randomuser.me/api/portraits/men/26.jpg')
+print(f"Imię: {user_info.name.first}")
+print(f"Nazwisko: {user_info.name.last}")
+print(f"Email: {user_info.email}")
+
+photo_url_pydantic = user_info.picture.large
+print(f"Link do zdjęcia: {photo_url_pydantic}")  # Link do zdjęcia: https://randomuser.me/api/portraits/men/21.jpg
+response_photo_pydantic = re.get(photo_url_pydantic)
+with open("user_photo_pydantic.jpg", "wb") as f:
+    f.write(response_photo_pydantic.content)
+print("Zdjęcie zostało zapisane")
