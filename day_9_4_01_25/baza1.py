5e08f7a76cc6e985eca18b22085008ad5a1db345
rjaniak <rajkonkret660@gmail.com>
2025-01-05 09:45:43 +0100

    [10 2] update baza1.py

--- day_9_4_01_25/baza1.py
+++ day_9_4_01_25/baza1.py
@@ -29,3 +29,19 @@
 # sqlite> .mode csv
 # sqlite> .import dane_person.csv users
 # sqlite>
+import sqlite3
+
+sql_connection = None
+try:
+    sql_connection = sqlite3.connect("sqlite_python.db")
+    cursor = sql_connection.cursor()
+    print("Baza danych została podłączona")
+except sqlite3.Error as e:
+    print("Błąd bazy danych", e)
+finally:
+    if sql_connection:
+        sql_connection.close()
+        print("Baza danych zostałą zamknięta")
+
+# Baza danych została podłączona
+# Baza danych zostałą zamknięta
