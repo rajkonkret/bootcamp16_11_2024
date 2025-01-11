875fb707f50a00e3c75f8b9a1854d9e86a487765
rjaniak <rajkonkret660@gmail.com>
2025-01-11 10:10:51 +0100

    [11 3] update baza_relacje.py

--- day_11_11_01_24/baza_relacje.py
+++ day_11_11_01_24/baza_relacje.py
@@ -12,3 +12,38 @@
 # wiele do wiele - kazdy rekord obydwu tabel mozę odnośić się do dowolnej liczby rekordów
 # W przypadku takich relacji wymagana jest trzecia tabela nazywana tabelą powiązań,
 # ponieważ systemy relacyjne nie mogą bezpośrednio obsługiwać takiej relacji.
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
+
+engine = create_engine('sqlite:///adress_book.db', echo=True)
+Base = declarative_base()
+
+
+# encje - klasy odwzorowujące tabele w bazie danych
+class Person(Base):
+    __tablename__ = 'person'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(String)
+    addresses = relationship(
+        'Address',
+        back_populates='person',
+        order_by='Address.email',
+        cascade='all, delete-orphan'
+    )
+
+    def __repr__(self):
+        return f"{self.name} (id={self.id})"
+
+
+class Address(Base):
+    __tablename__ = 'address'
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    person_id = Column(ForeignKey('person.id'))
+    person = relationship("Person", back_populates='addresses')
+
+    def __repr__(self):
+        return self.email
+
+Base.metadata.create_all(engine)
