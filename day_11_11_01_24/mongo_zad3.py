5d1a5db9d4a4607721617ad5a8ee3a2ebebc2ab0
rjaniak <rajkonkret660@gmail.com>
2025-01-11 15:45:44 +0100

    [11 12a] add mongo_zad3.py

---
+++ day_11_11_01_24/mongo_zad3.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+import certifi
+
+# python -m pip install "pymongo[srv]"==3.12
+# pip install pymongo[srv]==3.12
+uri = "mongodb+srv://rajkonkret660:2zkqLAEqgsle90O3@cluster0.pjuh61y.mongodb.net/?appName=Cluster0"
+
+client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print("Error:", e)
+# Pinged your deployment. You successfully connected to MongoDB!
+#
+# Process finished with exit code 0
