3048a4eef15bca6f237c9481c7a82f1cbd592cdf
rjaniak <rajkonkret660@gmail.com>
2025-01-11 15:29:36 +0100

    [11 11] add mongo_zad2.py

---
+++ day_11_11_01_24/mongo_zad2.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017")
+
+db = client['przykladowa_baza']
+kolekcja = db['uzytkownicy']
+
+# kolekcja.insert_one(
+#     {'imie': 'Jan', 'nazwisko': "Kowalski", 'wiek': 30}
+# )
+
+kolekcja.insert_many(
+    [
+        {'imie': 'Anna', 'nazwisko': 'Nowak', 'wiek': 25},
+        {'imie': 'Paweł', 'nazwisko': 'Wiśniewski', 'wiek': 19},
+    ]
+)
+
+for uzytkownik in kolekcja.find():
+    print(uzytkownik)
+
+print(kolekcja.find_one({"imie": "Jan"}))
+# {'_id': ObjectId('67827dd362a66e2a3d492026'), 'imie': 'Jan', 'nazwisko': 'Kowalski', 'wiek': 30}
+
+client.close()
+# {'_id': ObjectId('67827dd362a66e2a3d492026'), 'imie': 'Jan', 'nazwisko': 'Kowalski', 'wiek': 30}
+# {'_id': ObjectId('67827dd362a66e2a3d492026'), 'imie': 'Jan', 'nazwisko': 'Kowalski', 'wiek': 30}
+# {'_id': ObjectId('67827e665a61ecb2c57d54c6'), 'imie': 'Anna', 'nazwisko': 'Nowak', 'wiek': 25}
+# {'_id': ObjectId('67827e665a61ecb2c57d54c7'), 'imie': 'Paweł', 'nazwisko': 'Wiśniewski', 'wiek': 19}
