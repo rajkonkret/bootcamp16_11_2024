2c943e1e4f9ea0b86561389d75e2bdc3aa11b789
rjaniak <rajkonkret660@gmail.com>
2025-01-12 13:05:24 +0100

    [12 12a] update obliczenie_pi.py

--- day_12_12_01_24/obliczenie_pi.py
+++ day_12_12_01_24/obliczenie_pi.py
@@ -10,28 +10,28 @@ total_points_inside_circle = 0
 
 
 # oblicznie pi metodą monte-carlo
-# def monte_carlo_pi(num_samples):
-#     x = np.random.uniform(-1, 1, num_samples).astype(np.float32)
-#     y = np.random.uniform(-1, 1, num_samples).astype(np.float32)
+def monte_carlo_pi(num_samples):
+    x = np.random.uniform(-1, 1, num_samples).astype(np.float32)
+    y = np.random.uniform(-1, 1, num_samples).astype(np.float32)
+
+    inside_circle = x ** 2 + y ** 2 <= 1
+    num_inside_circle = np.sum(inside_circle)
+    pi_estimate = 4 * num_inside_circle / num_samples
+
+    return pi_estimate
+
 #
-#     inside_circle = x ** 2 + y ** 2 <= 1
-#     num_inside_circle = np.sum(inside_circle)
-#     pi_estimate = 4 * num_inside_circle / num_samples
+# def monte_carlo_pi(n):
+#     points_inside_circle = 0
 #
-#     return pi_estimate
-
-
-def monte_carlo_pi(n):
-    points_inside_circle = 0
-
-    for _ in range(n):
-        x = random.uniform(-1, 1)
-        y = random.uniform(-1, 1)
-
-        if x ** 2 + y ** 2 <= 1:
-            points_inside_circle += 1
-
-    return 4 * (points_inside_circle / n)
+#     for _ in range(n):
+#         x = random.uniform(-1, 1)
+#         y = random.uniform(-1, 1)
+#
+#         if x ** 2 + y ** 2 <= 1:
+#             points_inside_circle += 1
+#
+#     return 4 * (points_inside_circle / n)
 
 
 def no_threads(iterations):
@@ -43,6 +43,7 @@ def no_threads(iterations):
 
 def with_threads(iterations):
     num_thread = 8
+    # num_thread = 4
     iterations_per_thread = iterations // num_thread
     threads = []
 
@@ -73,11 +74,30 @@ def with_processes(iterations):
     iterations_per_process = iterations // num_processes
 
     start = time.time()
-    with multiprocessing.Pool(num_processes)
+    with multiprocessing.Pool(num_processes) as pool:
+        results = pool.map(monte_carlo_pi, [iterations_per_process] * num_processes)
+        pi = sum(results) / num_processes
+        end = time.time()
+        print(f"Z procesami: {pi}, czas {end - start}")
 
 
-iterations = 10_000_000
-# iterations = 50_000_000
+def with_thread_pool_executor(iterations):
+    num_threads = 8
+    iterations_per_thread = iterations // num_threads
+
+    start = time.time()
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        results = list(executor.map(monte_carlo_pi, [iterations_per_thread] * num_threads))
+        pi = sum(results) / num_threads
+
+    end = time.time()
+    print(f"Z ThreadPoolExecutor: {pi}, czas {end - start}")
+
+
+# iterations = 10_000_000
+iterations = 50_000_000
 if __name__ == '__main__':
     # no_threads(iterations)
-    with_threads(iterations)
+    # with_threads(iterations)
+    # with_processes(iterations)
+    with_thread_pool_executor(iterations)
