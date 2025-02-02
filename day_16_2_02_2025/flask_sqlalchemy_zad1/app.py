e361e097da460cad0df82470d53d9f5e54c8bace
rjaniak <rajkonkret660@gmail.com>
2025-02-02 10:11:05 +0100

    [16 3] update flask-sqlalchemy

--- day_16_2_02_2025/flask_sqlalchemy_zad1/app.py
+++ day_16_2_02_2025/flask_sqlalchemy_zad1/app.py
@@ -13,10 +13,40 @@ class Vendor(db.Model):
     discount = db.Column(db.Integer)
     active = db.Column(db.Boolean)
 
+    def __repr__(self):
+        return f"Vendor: {self.id}/{self.name}"
+
+
 @app.route("/")
 def index():
     db.create_all()
-    return 'Hello'
+
+    # v1 = Vendor(id=1, name='Microsoft', discount=0, active=True)
+    # db.session.add(v1)
+    # db.session.commit()
+    # v1 = Vendor(id=2, name='Samsung', discount=5, active=True)
+    # db.session.add(v1)
+    # db.session.commit()
+    # v1 = Vendor( name='Samsung', discount=5, active=True) # automatyczny id
+    # db.session.add(v1)
+    # db.session.commit()
+    # v1 = Vendor( name='Apple', discount=3, active=True) # automatyczny id
+    # db.session.add(v1)
+    # db.session.commit()
+
+    # vendors = Vendor.query.all()
+    # vendors = Vendor.query.filter(Vendor.discount > 0).all()
+    # vendors = Vendor.query.filter(Vendor.discount > 3).all()
+    # vendors = Vendor.query.filter(Vendor.name.like('%s%')).all()
+    vendors = Vendor.query.filter(Vendor.name.like('%S%')).all()
+
+    ret = ''
+    for v in vendors:
+        # ret += v.name + '<br>'
+        ret += str(v) + '<br>'
+
+    # return 'Hello'
+    return f'Hello<br>{ret}'
 
 
 if __name__ == '__main__':
