8259fd77e0dd91c8bedfa61fd473afc719bddcf4
rjaniak <rajkonkret660@gmail.com>
2025-02-02 15:02:27 +0100

    [16 11] add flask-mail

---
+++ day_16_2_02_2025/flask_send_mail/app.py
@@ -0,0 +1,35 @@
+import os
+
+from flask import Flask, render_template
+from dotenv import load_dotenv
+from flask_mail import Mail, Message
+
+#  pip install python-dotenv
+load_dotenv()
+app = Flask(__name__)
+app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT'] = ['Apka']
+app.config['FLASKY_MAIL_SENDER'] = os.getenv('MAIL_USERNAME')
+
+mail = Mail(app)
+
+
+@app.route("/")
+def index():
+    return '<h1>Hello</h1>'
+
+
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    mail.send(msg)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
