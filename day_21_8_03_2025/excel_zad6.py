52f75af57f8cfdae21d1c4d232888ab7fb7189de
rjaniak <rajkonkret660@gmail.com>
2025-03-08 16:18:26 +0100

    [21 25] fix excel_zad6.py

--- day_21_8_03_2025/excel_zad6.py
+++ day_21_8_03_2025/excel_zad6.py
@@ -1,5 +1,12 @@
 import pandas as pd
+with open("tabela_przestawna3.xlsx", "rb") as f:
+    content = f.read()
+with open("tabela_przestawna3.xlsx", "rb") as f:
+    content = f.read(500)  # Pobieramy pierwsze 500 bajtów
 
-df = pd.read_excel("tabela_przestawna2.xlsx", skiprows=3)
+print(content[:200])  # Podgląd pierwszych 200 bajtów
+
+print("Rozmiar pliku:", len(content), "bajtów")
+df = pd.read_excel("tabela_przestawna3.xlsx", skiprows=3, engine="openpyxl")
 
 print(df.head())
