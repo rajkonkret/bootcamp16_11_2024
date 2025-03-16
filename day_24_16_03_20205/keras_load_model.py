6dfcbf4415b58827e291639e677f96f0f7a98d96
rjaniak <rajkonkret660@gmail.com>
2025-03-16 15:56:50 +0100

    [24 10] add tensorflow/keras

---
+++ day_24_16_03_20205/keras_load_model.py
@@ -0,0 +1,27 @@
+import os
+
+import numpy as np
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"  # przed importowaniem kerasa
+from tensorflow.keras.models import load_model
+
+model = load_model("model_or.h5")
+# kompilacja modelu
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+model.summary()
+
+# dane wejściowe
+X = np.array(
+    [[0, 0],
+     [0, 1],
+     [1, 0],
+     [1, 1]]
+)
+
+
+# testowanie mmodelu
+predictions = model.predict(X)
+predictions = (predictions > 0.5).astype(int)
+
+for i in range(len(X)):
+    print(f"{X[i]} {predictions[i]} )")
---
+++ day_24_16_03_20205/tensorflow_zad1.py
@@ -0,0 +1,70 @@
+import os
+
+import numpy as np
+
+# pip install tensorflow
+# pip uninstall tensorflow
+# pip install tensorflow-cpu
+
+# pip install tensorflow-macos
+# pip install tensorflow-metal (gpu)
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+import tensorflow as tf
+from tensorflow import keras
+from keras import Input
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+print("Dostępne urządzenia:")
+print(tf.config.list_physical_devices())
+print("Czy GPU jest dostępne:", tf.config.list_physical_devices('GPU'))
+
+# dane wejściowe
+X = np.array(
+    [[0, 0],
+     [0, 1],
+     [1, 0],
+     [1, 1]]
+)
+
+# dane do nauki
+y_xor = np.array([[0], [1], [1], [0]])
+y_and = np.array([[0], [0], [0], [1]])
+y_or = np.array([[0], [1], [1], [1]])
+
+
+# trening
+def train_model(y_train, logic_type):
+    print(f"Uczenie modelu dla operacji: {logic_type}\n")
+
+    # definiujemy model
+    model = Sequential([
+        Input(shape=(2,)),
+        Dense(4, activation="relu", ),  # warstwa ukryta, 4 nerony, funkcla relu
+        Dense(1, activation="sigmoid")  # warstwa wyjściowa, z funkcją sigmoidalną
+    ])
+
+    # kompilacja modelu
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+    # trenowanie modelu
+    with tf.device('/GPU:0'):
+        model.fit(X, y_train, epochs=500, verbose=0)
+
+    # testowanie mmodelu
+    predictions = model.predict(X)
+    predictions = (predictions > 0.5).astype(int)
+
+    print(f"Przewidywanie wyników dla operacji {logic_type}")
+    for i in range(len(X)):
+        print(f"{X[i]} {predictions[i][0]} )oczekiwanie: {y_train[i][0]}")
+
+    return model
+
+
+model_xor = train_model(y_xor, "XOR")
+model_and = train_model(y_and, "AND")
+model_or = train_model(y_or, "OR")
+
+model_or.save("model_or.keras")
