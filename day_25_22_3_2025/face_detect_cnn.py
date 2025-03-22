3812dc26129105c6c581d782dbd1db041ab9c0cf
rjaniak <rajkonkret660@gmail.com>
2025-03-22 15:52:04 +0100

    [25 11]add face_detect.py dlib cnn

---
+++ day_25_22_3_2025/face_detect_cnn.py
@@ -0,0 +1,28 @@
+import cv2
+import dlib
+import matplotlib.pyplot as plt
+
+cnn_model_path = "mmod_human_face_detector.dat"
+
+image_path = "obraz.jpg"
+image = cv2.imread(image_path)
+
+if image is None:
+    raise ValueError("Nie udało się wczytać obrazu")
+
+rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+cnn_detector = dlib.cnn_face_detection_model_v1(cnn_model_path)
+
+detections = cnn_detector(rgb_image, upsample_num_times=1)
+
+for d in detections:
+    rect = d.rect
+    x, y, w, h = rect.left(), rect.top(), rect.width(), rect.height()
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+plt.figure(figsize=(10, 2))
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.title(f"Dokłądniejszy model - wykryto twarzy: {len(detections)}")
+plt.axis("off")
+plt.show()
